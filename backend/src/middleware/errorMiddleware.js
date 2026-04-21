713dd253662d7baa1c82c56476ef46e03331c5bc
Vivan <pesu1ug23am354@pesu.pesu.edu>
2026-04-21 12:36:58 +0530

    Removed duplicate outer backend, frontend, and docs folders

--- backend/src/middleware/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-const { failure } = require('../utils/apiResponse');
-
-const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  return failure(res, err.message || 'Internal server error', err.statusCode || 500);
-};
-
-module.exports = errorHandler;
